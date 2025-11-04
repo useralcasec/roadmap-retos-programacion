85a8ba705a2b3edadc6d39a74a78274d0527155c
useralcasec <useralcasec@gmail.com>
2025-11-04 01:35:44 +0100

    usuarioalcasec.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/usuarioalcasec.py
@@ -0,0 +1,70 @@
+# https://www.python.org/
+
+# Comentario de una linea
+
+"""
+Este es un comentario
+de múltiples líneas
+"""
+
+'''
+Este es otro comentario
+de múltiples líneas
+'''
+
+"""
+Nombres de variables en Python
+Según la PEP 8 los nombres de las variables de Python deben de escribirse en snake_case. Además se deben de cumplir las siguientes características:
+
+Tienen que empezar por una letra o barra baja.
+El uso de keywords como nombres está prohibido.
+Los nombres deben de ser descriptivos.
+Deben de estar en minúsculas y separando palabras por barras bajas ‘_’.
+Las constantes se escriben en mayúsculas y SNAKE_CASE.
+
+Fuente: https://elpythonista.com/variables-python
+"""
+
+my_variable = "Esto es una variable"
+MY_CONSTANT = "Esto es una constante"
+
+# TIPOS DE DATOS PRIMITIVOS
+# Entero (int)
+"""
+Representa números enteros, positivos o negativos, sin parte fraccionaria.
+"""
+entero = 10
+print(entero)
+print(type(entero)) # Imprime el tipo de dato de la variable, en este caso 'int'
+
+# Flotante (float)
+"""
+Representa números decimales, es decir, números con parte fraccionaria.
+"""
+flotante = 3.14
+print(flotante)
+print(type(flotante)) # Imprime el tipo de dato de la variable, en este caso 'float'
+
+# Cadena de caracteres (str)
+"""
+Representa una secuencia de caracteres. Puede contener letras, números, y otros caracteres.
+"""
+cadena = "¡Hola, Python!"
+print(cadena)
+print(type(cadena)) # Imprime el tipo de dato de la variable, en este caso 'str'
+
+# Booleano (bool)
+"""
+Representa valores de verdadero (True) o falso (False).
+"""
+booleano = True
+print(booleano) # también puede ser False
+print(type(booleano)) # Imprime el tipo de dato de la variable, en este caso 'bool'
+
+# Ejercicio: Imprime una cadena de caracteres (el mítico "Hola, Python!")
+print("¡Hola, Python!") # Imprime la cadena de caracteres "¡Hola, Python!"
+'''
+Una pequeña anotación para aquel ineterasdo que haya llegado hasta aqui, yo esoy usando
+ VSCode como editor de código y me he enamorado de el siguiente tema:
+ en Configuración ve a "themes" y ponte uno que se llama Monokai, a mi me encanta jejejejej <3.
+ '''
